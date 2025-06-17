156a7e9ad837ff77408937178238ff127256aca4
Ravikumar-Pawar <ravi.ravipawar17@gmail.com>
2025-06-17 14:53:13 +0530

    SortHelp added

--- Views/Sort/SortWindow.xaml.cs
+++ Views/Sort/SortWindow.xaml.cs
@@ -19,5 +19,39 @@ namespace KannadaNudiEditor.Views.Sort
         {
             this.Close();
         }
+
+
+
+        private void InfoButton_Click(object sender, RoutedEventArgs e)
+        {
+            string message =
+        @"**Sort By**
+If sorting a list, choose Paragraphs, Headings, or Fields.
+If sorting a table and you have a header row, choose by header name.
+If you don't have a header, choose by column number.
+
+**Type**
+Choose between Text, Number, or Date.
+
+**Using**
+Choose Paragraphs.
+
+**Ascending or Descending**
+Choose sorting method.
+
+**My list has**
+Choose whether your list or table has headers.
+
+**Options**
+Choose sort options.
+
+• Where to separate fields (Tabs, Comma, Other)
+• Sort options (Sort column only, Case sensitive)
+• Sorting language";
+
+            MessageBox.Show(message, "Sort Help", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+
     }
 }
